6faabcfffd053fa64b4ae4e11dbec27388926c01
Rishab Angadi <rishab@Rishabs-MacBook-Air.local>
2024-01-27 12:39:02 +0530

    Updated protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,13 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
+
   const isPublic = path === "/login" || path === "/signup";
+
   const token = request.cookies.get("token")?.value || "";
-  if (isPublic && token) {
+  if ((isPublic && token) || (path === "/" && token)) {
     return NextResponse.redirect(new URL("/profile", request.nextUrl));
-  }
-  if (!isPublic && !token) {
+  } else if (!isPublic && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
 }
